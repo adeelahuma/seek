a7defdaadeb09c8fbf9a174d7d0aa4be85bb28d7
adeelahuma <ahuma@vt.edu>
2018-10-29 12:19:09 -0400

    comment bokeh sampledata

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import random
 import bokeh.sampledata
-from bokeh.sampledata import us_states, us_counties, unemployment
+# from bokeh.sampledata import us_states, us_counties, unemployment
 from bokeh.sampledata.stocks import AAPL
 from bokeh.plotting import figure
 from bokeh.embed import components
@@ -94,55 +94,55 @@ def create_bar_chart(data, title, x_title, y_title,
 
 def job_map():
 
-    states = us_states.data.copy()
-    print(states)
-
-    counties = us_counties.data.copy()
-
-    print(counties)
-
-    unemployment_us = unemployment.data
-
-    print(unemployment_us)
-
-    del states["HI"]
-    del states["AK"]
-
-    EXCLUDED = ("ak", "hi", "pr", "gu", "vi", "mp", "as")
-
-    state_xs = [states[code]["lons"] for code in states]
-    state_ys = [states[code]["lats"] for code in states]
-
-    county_xs = [counties[code]["lons"] for code in counties if counties[code]["state"] not in EXCLUDED]
-    county_ys = [counties[code]["lats"] for code in counties if counties[code]["state"] not in EXCLUDED]
-
-    colors = ["#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043"]
-
-    county_colors = []
-    for county_id in counties:
-        if counties[county_id]["state"] in EXCLUDED:
-            continue
-        try:
-            rate = unemployment_us[county_id]
-            idx = int(rate / 6)
-            county_colors.append(colors[idx])
-        except KeyError:
-            county_colors.append("black")
-
-    p = figure(title="Job Map", toolbar_location="left",
-               plot_width=1100, plot_height=700)
-
-    p.patches(county_xs, county_ys,
-              fill_color=county_colors, fill_alpha=0.7,
-              line_color="white", line_width=0.5)
-
-    p.patches(state_xs, state_ys, fill_alpha=0.0,
-              line_color="#884444", line_width=2, line_alpha=0.3)
-
-    html = file_html(p, CDN, 'job')
-
-    with open("templates/job_map.html", 'w') as f:
-        f.write(html)
+    # states = us_states.data.copy()
+    # print(states)
+    #
+    # counties = us_counties.data.copy()
+    #
+    # print(counties)
+    #
+    # unemployment_us = unemployment.data
+    #
+    # print(unemployment_us)
+    #
+    # del states["HI"]
+    # del states["AK"]
+    #
+    # EXCLUDED = ("ak", "hi", "pr", "gu", "vi", "mp", "as")
+    #
+    # state_xs = [states[code]["lons"] for code in states]
+    # state_ys = [states[code]["lats"] for code in states]
+    #
+    # county_xs = [counties[code]["lons"] for code in counties if counties[code]["state"] not in EXCLUDED]
+    # county_ys = [counties[code]["lats"] for code in counties if counties[code]["state"] not in EXCLUDED]
+    #
+    # colors = ["#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043"]
+    #
+    # county_colors = []
+    # for county_id in counties:
+    #     if counties[county_id]["state"] in EXCLUDED:
+    #         continue
+    #     try:
+    #         rate = unemployment_us[county_id]
+    #         idx = int(rate / 6)
+    #         county_colors.append(colors[idx])
+    #     except KeyError:
+    #         county_colors.append("black")
+    #
+    # p = figure(title="Job Map", toolbar_location="left",
+    #            plot_width=1100, plot_height=700)
+    #
+    # p.patches(county_xs, county_ys,
+    #           fill_color=county_colors, fill_alpha=0.7,
+    #           line_color="white", line_width=0.5)
+    #
+    # p.patches(state_xs, state_ys, fill_alpha=0.0,
+    #           line_color="#884444", line_width=2, line_alpha=0.3)
+    #
+    # html = file_html(p, CDN, 'job')
+    #
+    # with open("templates/job_map.html", 'w') as f:
+    #     f.write(html)
 
     # output_file("choropleth.html", title="choropleth.py example")
 
