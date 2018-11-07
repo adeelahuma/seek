5d2ece6d552d2e4720febeae29e2ea44caefe07b
adeelahuma <ahuma@vt.edu>
2018-11-06 22:26:21 -0500

    notebook updated graph for employee trend updated graph for employee turn over added

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 
 import random
-import bokeh.sampledata
-# from bokeh.sampledata import us_states, us_counties, unemployment
-# from bokeh.sampledata.stocks import AAPL
-from bokeh.plotting import figure
+from flask import Flask, render_template, request
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
-from bokeh.embed import file_html
-from bokeh.resources import CDN
-from flask import Flask, render_template
-from bokeh.io import show
-# import pandas as pd
-
+from bokeh.models import ColumnDataSource
+from bokeh.models.tools import HoverTool
+from bokeh.palettes import Spectral3
+import numpy as np
 
 app = Flask(__name__)
+
+df = pd.read_pickle('data/application_data.pickle')
+company_names = np.unique(df.company_name).tolist()
 
 
 @app.route("/")
@@ -20,82 +20,167 @@ def begin():
     return render_template('index.html')
 
 
+def plot_trend(c_name):
+    # step-1: filter data by company
+    # step-2: plot
+    # c_name = 'Amazon'
+
+    plot_df = df[df.company_name == c_name]
+
+
+    ## bokeh plot
+
+    source = ColumnDataSource(plot_df)
+
+    p = figure(x_axis_type='datetime')
+
+    p.line(x='year_month_01',
+           y='emp_begin',
+           source=source,
+           legend='# of Employees (Start of month)',
+           color=Spectral3[0],
+           line_width=2)
+
+    p.line(x='year_month_01',
+           y='emp_end',
+           source=source,
+           legend='# of Employees (End of month)',
+           color='red', line_width=2)
+
+    p.title.text = 'Employee growth trend for ' + c_name
+    p.xaxis.axis_label = 'Date'
+    p.yaxis.axis_label = 'Number of employees on platform'
+
+    hover = HoverTool()
+    hover.tooltips = [
+        ('Employees at the start of month', '@emp_begin'),
+        ('Employees at the end of month', '@emp_end'),
+        ('Month/Year', '@year_month_01 ')
+    ]
+
+    p.add_tools(hover)
+
+    return p
+
+
+# function that'll be used during plotting so that left/join bars can be of different colors
+def get_turn_over_data_by_color(df):
+    df_left = df[['company_name', 'year_month_01', 'left_per_month']]
+    df_left.rename(columns={'left_per_month': 'emp'}, inplace=True)
+    df_left['color'] = 'red'
+    df_left['legend'] = 'left'
+
+    df_joined = df[['company_name', 'year_month_01', 'joined_per_month']]
+    df_joined.rename(columns={'joined_per_month': 'emp'}, inplace=True)
+    df_joined['color'] = 'blue'
+    df_joined['legend'] = 'joined'
+
+    df_emp_count = pd.concat([df_joined, df_left], axis=0)
+
+    return df_emp_count
+
+
+def emp_turn_over_plot(company, negativeAxis=False):
+
+    plot_df = df[df.company_name == company]
+
+    df_turn_over_plot = get_turn_over_data_by_color(plot_df)
+
+    if negativeAxis:
+        # for left employees multiply with -1 so that they appear on negative y-axis
+        df_turn_over_plot['modified_emp'] = np.where(df_turn_over_plot.color == 'red',
+                                                     df_turn_over_plot.emp * -1,
+                                                     df_turn_over_plot.emp * 1)
+
+    source = ColumnDataSource(df_turn_over_plot)
+    p = figure(x_axis_type='datetime')
+
+    if negativeAxis:
+        p.vbar(x='year_month_01', width=5,
+               top='modified_emp',
+               source=source,
+               color='color',
+               legend='legend')
+    else:
+        p.vbar(x='year_month_01', width=5,
+               top='emp',
+               source=source,
+               color='color',
+               legend='legend')
+
+    p.title.text = 'Employee Turn-over for ' + company
+    p.xaxis.axis_label = 'Date'
+    p.yaxis.axis_label = 'Number of employees left or joined'
+
+    hover = HoverTool()
+    hover.tooltips = [
+        ('Employees(left/joined)', '@emp'),
+        ('Month/Year', '@year_month_01 ')
+    ]
+
+    p.add_tools(hover)
+
+    return p
+
+@app.route('/employee_turn_over')
+def emp_turn_over():
+
+    company = request.args.get('company_name')
+    print('name: ', company)
+
+    if company == None:
+        company = 'Apple'
+
+    p1 = emp_turn_over_plot(company, False)
+    script1, div1 = components(p1)
+
+    p2 = emp_turn_over_plot(company, True)
+    script2, div2 = components(p2)
+
+    return render_template('employee_turn_over.html',
+                           the_div1=div1, the_script1=script1,
+                           the_div2=div2, the_script2=script2,
+                           company_names=company_names, selected_company=company)
+
+
 @app.route('/trend')
-def trend_html():
-    return render_template('trend.html')
-
-
 def trend_graph():
-
-    df = pd.DataFrame(AAPL)
-    df['date'] = pd.to_datetime(df['date'])
-
-    # create a new plot with a datetime axis type
-    p = figure(title="Job Trend", plot_width=800, plot_height=250, x_axis_type="datetime")
-
-    p.line(df['date'], df['close'], color='navy', alpha=0.5)
-
-    html = file_html(p, CDN, 'trend')
-    # script, div = components(p)
     #
-    # return render_template('trend.html', the_div=div, the_script=script)
-    with open("templates/trend.html", 'w') as f:
-        f.write(html)
-
-
-def create_hover_tool():
-    #yet to be coded
-    return None
-
-
-def create_bar_chart(data, title, x_title, y_title,
-                     hover_tool=None, width=1200, height=1000):
-    # source = ColumnDataSource(data)
-    # print(data[x_title])
-    # xdr = FactorRange(data[x_title])
-    # ydr = Range1d(start=0, end=max(data[y_title]) * 1.5)
+    # df = pd.DataFrame(AAPL)
+    # df['date'] = pd.to_datetime(df['date'])
     #
-    # tools = []
-    # if hover_tool:
-    #     tools = [hover_tool, ]
+    # # create a new plot with a datetime axis type
+    # p = figure(title="Job Trend", plot_width=800, plot_height=250, x_axis_type="datetime")
     #
-    # plot = figure(title=title, x_range=xdr, y_range=ydr, plot_width=width,
-    #               plot_height=height, h_symmetry=False, v_symmetry=False, min_border=0,
-    #               toolbar_location="above", tools=tools, responsive=True,
-    #               outline_color="#666666")
-    #
-    # # glyph = VBar(x=x_title, top=y_title, bottom=0, width=.8,
-    # #              fill_color="#e12127")
-    # # plot.add_glyph(source, glyph)
-    #
-    #
-    #
-    # xaxis = LinearAxis()
-    # yaxis = LinearAxis()
-    #
-    # plot.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
-    # plot.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
-    # plot.toolbar.logo = None
-    # plot.min_border_top = 0
-    # plot.xgrid.grid_line_color = None
-    # plot.ygrid.grid_line_color = "#999999"
-    # plot.yaxis.axis_label = "Bugs found"
-    # plot.ygrid.grid_line_alpha = 0.1
-    # plot.xaxis.axis_label = "Days after app deployment"
-    # plot.xaxis.major_label_orientation = 1
+    # p.line(df['date'], df['close'], color='navy', alpha=0.5)
+    p = plot_trend()
 
-    plot = figure(plot_width=400, plot_height=400)
+    script, div = components(p)
 
-    # add a circle renderer with a size, color, and alpha
-    plot.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
-
-    return plot
+    return render_template('trend.html', the_div=div, the_script=script)
 
 
+@app.route('/employee_growth_trend')
+def emp_growth_trend():
+
+    company = request.args.get('company_name')
+    print('name: ', company)
+
+    if company == None:
+        company = 'Apple'
+
+    p = plot_trend(company)
+
+    script, div = components(p)
+
+    return render_template('employee_growth_trend.html',
+                           the_div=div, the_script=script,
+                           company_names=company_names, selected_company=company)
+
+
+@app.route('/job_map')
 def job_map():
-
-    print('job map function')
-
+    print('job trend called')
     # states = us_states.data.copy()
     # print(states)
     #
@@ -141,26 +226,17 @@ def job_map():
     # p.patches(state_xs, state_ys, fill_alpha=0.0,
     #           line_color="#884444", line_width=2, line_alpha=0.3)
     #
-    # html = file_html(p, CDN, 'job')
     #
-    # with open("templates/job_map.html", 'w') as f:
-    #     f.write(html)
-
-    # output_file("choropleth.html", title="choropleth.py example")
-
-    # show(p)
-    # show(plot)
     # script, div = components(p)
+    #
     # return render_template('job_map.html', the_div=div, the_script=script)
 
 
-@app.route('/job_map')
-def job_html():
-    return render_template('job_map.html')
+# @app.before_request
+# def before_request():
+#     init_data()
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    # job_map()
-    # trend_graph()
-    app.run(port=33507)
+    # app.run(port=33507)
+    app.run(debug=True)
